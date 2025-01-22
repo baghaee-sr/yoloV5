d27f4e702fbc746ec0727139315ea9272a9e23db
Saeed Baghaee Ravari <baghaee.sr@gmail.com>
2025-01-22 18:15:03 +0330

    Update common.py add SELayer

--- models/common.py
+++ models/common.py
@@ -734,7 +734,25 @@ class Detections:
         self.print()  # override print(results)
         return ''
 
+class SELayer(nn.Module):
+    def __init__(self, c1, r=16):
+        super(SELayer, self).__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.l1 = nn.Linear(c1, c1 // r, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+        self.l2 = nn.Linear(c1 // r, c1, bias=False)
+        self.sig = nn.Sigmoid()
 
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avgpool(x).view(b, c)
+        y = self.l1(y)
+        y = self.relu(y)
+        y = self.l2(y)
+        y = self.sig(y)
+        y = y.view(b, c, 1, 1)
+        return x * y.expand_as(x)
+      
 class Classify(nn.Module):
     # Classification head, i.e. x(b,c1,20,20) to x(b,c2)
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1):  # ch_in, ch_out, kernel, stride, padding, groups
